aeb41552c5da8db6e7c44a330e4b36af5a9da12a
Graydon Strachan <glstrachan@outlook.com>
2026-02-04 15:24:20 -0800

    Changes

--- src/pick_items.h
+++ src/pick_items.h
@@ -9,19 +9,19 @@ typedef struct {
 } Item_Data;
 
 #define ITEM_LIST(X) \
-    X(ITEM_NONE,              NULL,               NULL)                \
-    X(ITEM_VACUUM,            "vacuum",           "Sucks up dirt")     \
-    X(ITEM_BOOKS,             "books",            "Knowledge")         \
-    X(ITEM_KNIVES,            "knives",           "Sharp")             \
-    X(ITEM_LOLLIPOP,          "lollipop",         "Sweet treat")       \
-    X(ITEM_COMPUTER,          "computer",         "Beep boop")         \
-    X(ITEM_VASE,              "vase",             "Holds flowers")     \
-    X(ITEM_COMIC_BOOK,        "comic book",       "Pow! Zap!")         \
-    X(ITEM_FOOTBALL,          "football",         "Throw it long")     \
-    X(ITEM_AK47,              "ak47",             "Fully automatic")   \
-    X(ITEM_GIRLSCOUT_COOKIES, "girlscout cookies", "Thin mints!")      \
-    X(ITEM_STEAK,             "steak",            "Medium rare")       \
-    X(ITEM_GAME_CONSOLE,      "game console",     "Press start")
+    X(ITEM_NONE,              NULL,                NULL)                \
+    X(ITEM_VACUUM,            "vacuum",            "Sucks up dirt")     \
+    X(ITEM_BOOKS,             "books",             "Knowledge")         \
+    X(ITEM_KNIVES,            "knives",            "Sharp")             \
+    X(ITEM_LOLLIPOP,          "lollipop",          "Sweet treat")       \
+    X(ITEM_COMPUTER,          "computer",          "Beep boop")         \
+    X(ITEM_VASE,              "vase",              "Holds flowers")     \
+    X(ITEM_COMIC_BOOK,        "comic book",        "Pow! Zap!")         \
+    X(ITEM_FOOTBALL,          "football",          "Throw it long")     \
+    X(ITEM_AK47,              "ak47",              "Fully automatic")   \
+    X(ITEM_GIRLSCOUT_COOKIES, "girlscout cookies", "Thin mints!")       \
+    X(ITEM_STEAK,             "steak",             "Medium rare")       \
+    X(ITEM_GAME_CONSOLE,      "game console",      "Press start")
 
 #define X(id, name, desc) id,
 Enum(Item_Type, uint32_t,
